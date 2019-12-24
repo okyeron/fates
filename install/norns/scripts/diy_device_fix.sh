c0f792aac98e24b3a73e87a1a3869d11e2a33b0a
okyeron <1909661+okyeron@users.noreply.github.com>
2019-12-24 15:12:59 -0800

    add crone fix to device hack

--- install/norns/scripts/diy_device_fix.sh
+++ install/norns/scripts/diy_device_fix.sh
@@ -1,5 +1,6 @@
 cd ~
 cd /home/we/norns
+sudo cp -f /home/we/fates/install/norns/files/crone/wscript /home/we/norns/crone/wscript
 sudo cp -f /home/we/fates/install/norns/files/device/device_monitor.c /home/we/norns/matron/src/device/device_monitor.c
 ./waf configure
 ./waf
