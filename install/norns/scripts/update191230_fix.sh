869d64c2ce4e520049e1149f5a643207bb5831ab
okyeron <1909661+okyeron@users.noreply.github.com>
2020-01-02 00:40:47 -0600

    Update update191230_fix.sh

--- install/norns/scripts/update191230_fix.sh
+++ install/norns/scripts/update191230_fix.sh
@@ -1,5 +1,7 @@
 cd /home/we/norns
 git pull
+sudo cp -f /home/we/fates/install/norns/files/crone/wscript /home/we/norns/crone/wscript
 sudo cp -f /home/we/fates/install/norns/files/191230/update.sh /home/we/norns/191230/update.sh
 ./waf configure
-./waf
+cd /home/we/norns/update/191230
+./update.sh
